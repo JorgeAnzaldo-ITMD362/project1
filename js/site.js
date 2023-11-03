ada30603b7da8e27b760973e9b2944c07b6cc524
Jorge Anzaldo <jaanzaldo12@gmail.com>
2023-11-02 20:15:43 -0500

    added javascript feature
    Makes a comment on which album is picked

--- js/site.js
+++ js/site.js
@@ -1,3 +1,11 @@
 jQuery(function($) {
   $('html').addClass('hasjs');
+  $('#submit').on("click", function(){
+	
+	if ($('#lateralus').is(':checked')){
+		javascript:alert("Lateralus was the correct answer. You've been accepted to the mailing list.");
+	} else {
+		javascript:alert("We'll assume that album choice was a misclick.");
+	}
+  });
 });
